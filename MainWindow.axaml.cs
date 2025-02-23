4a1661698823190073e225cae66bf19ea7ec94da
anasteshi <anasteshi13@gmail.com>
2025-02-23 17:03:28 +0200

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+
+namespace AvaloniaApplication
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+
+    }
+}
