08132b457a9c83232620e9a4c9c8e57ed67ca043
anasteshi <anasteshi13@gmail.com>
2025-02-23 17:40:31 +0200

    Delete unnecessary assets
    Deleted old pngs that are no longer needed

--- obj/Debug/net9.0/AvaloniaApplication.AssemblyInfo.cs
+++ obj/Debug/net9.0/AvaloniaApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1661698823190073e225cae66bf19ea7ec94da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5865e82cdca6d239538205ca628ea955312caaac")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
